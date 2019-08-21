56052700c23f33c6bcfc69b4908793550dd33801
davideastmond <davebuck1981@gmail.com>
2019-08-20 20:31:44 -0400

    refactor code

--- server.js
+++ server.js
@@ -66,31 +66,44 @@ app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
 
-function getData (arrayOfTopics) {
-	return new Promise((masterResolve, reject) => {
-		let urls = arrayOfTopics.map((element) => {
+function getData (arrayOfTags) {
+	return new Promise((masterResolve) => {
+		let urls = arrayOfTags.map((element) => {
 			return `https://hatchways.io/api/assessment/blog/posts?tag=${element}`;
 		});
 		
-		let posts = [];
-		let promises = urls.map((eachURL) => {
-			return new Promise((resolve, reject) => {
-				axios.get(eachURL)
-				.then((result) => {
-					resolve(result.data.posts);
-					result.data.posts.forEach((p) => {
-						posts.push(p);
-					});
-					//console.log("POSTS", result.data.posts);
-				});
-			
-			});
-		});
+		let postPromiseData = getPromisesArray(urls);
 	
-		Promise.all(promises)
-		.then((values) => {
-			console.log("POSTS", posts);
-			masterResolve(posts);
+		// Complete all the axios get requests, and when complete, resolve everything, returning an array of posts
+		Promise.all(postPromiseData.promises)
+		.then(() => {
+			// This should filter out duplicate posts by post id (from: https://dev.to/vuevixens/removing-duplicates-in-an-array-of-objects-in-js-with-sets-3fep )
+			let uniquePosts = Array.from(new Set(postPromiseData.posts.map(a => a.id )))
+			.map(id => {
+				return postPromiseData.posts.find(a => a.id === id);
+			})
+;			masterResolve(uniquePosts);
 		});
 	});
+}
+
+function getPromisesArray(urls) {
+	// This is going to return an object - first element a promise arrray, second an array of posts
+	let returnPosts = [];
+	let promises = urls.map((eachURL) => {
+		return new Promise((resolve) => {
+			axios.get(eachURL)
+			.then((result) => {
+				resolve(result.data.posts);
+
+				// Clean up the post data and store it in an array
+				result.data.posts.forEach((p) => {
+					returnPosts.push(p);
+				});
+			});
+		});
+	});
+
+	
+	return { promises: promises, posts: returnPosts };
 }
